2bec89c52ee9b164872c312d3a8ac22505c2497e
rmu <r2hkri@gmail.com>
2018-09-04 00:38:13 -0500

    somehow fix segfaults (???)

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
-#include <iostream>
-
 #include <gtk/gtk.h>
 #include <gio/gunixoutputstream.h>
 
 typedef struct mouse_info {
 	double x;
 	double y;
-	bool reset;
-	int color_index = 0;
+	int reset;
+	int color_index;
 } mouse_info;
 
 typedef struct ss_info {
@@ -20,7 +18,7 @@ typedef struct gtk_info {
 	cairo_surface_t *surface;
 	GtkWidget *window;
 	GtkWidget *drawing_area;
-	bool saving;
+	int saving;
 } gtk_info;
 
 static mouse_info mh;
@@ -46,7 +44,7 @@ static void draw_brush(GtkWidget *widget, gdouble x, gdouble y)
 	if (mh.reset)
 	{
 		cairo_move_to(cr, x, y);
-		mh.reset = false;
+		mh.reset = 0;
 	}
 	else
 	{
@@ -68,7 +66,7 @@ static gboolean button_press_cb(GtkWidget *widget, GdkEventButton *event, gpoint
 {
 	if (gtk.saving)
 	{
-		return true;
+		return 1;
 	}
 	// draw button (lmb)
 	else if (event->button == GDK_BUTTON_PRIMARY)
@@ -76,24 +74,24 @@ static gboolean button_press_cb(GtkWidget *widget, GdkEventButton *event, gpoint
 		draw_brush(widget, event->x, event->y);
 	}
 	// output and quit button (rmb)
-	else if (event->button == GDK_BUTTON_SECONDARY && ss.width != NULL)
+	else if (event->button == GDK_BUTTON_SECONDARY)
 	{
 		// prevent any more events from coming out
-		gtk.saving = true;
+		gtk.saving = 1;
 		GdkPixbuf* buf = gdk_pixbuf_get_from_surface(gtk.surface, 0, 0, ss.width, ss.height);
-		GOutputStream *ostream = g_unix_output_stream_new(1, true);
-		gdk_pixbuf_save_to_stream(buf, ostream, ss.type, NULL, NULL);
+		GOutputStream *ostream = g_unix_output_stream_new(1, 1);
+		gdk_pixbuf_save_to_streamv(buf, ostream, ss.type, NULL, NULL, NULL, NULL);
 
 		clean_and_close();
 	}
-	return true;
+	return 1;
 }
 
 static gboolean motion_notify_cb(GtkWidget *widget, GdkEventMotion *event, gpointer data)
 {
 	if (gtk.saving)
 	{
-		return true;
+		return 1;
 	}
 	else if (event->state & GDK_BUTTON1_MASK)
 	{
@@ -101,9 +99,9 @@ static gboolean motion_notify_cb(GtkWidget *widget, GdkEventMotion *event, gpoin
 	}
 	else
 	{
-		mh.reset = true;
+		mh.reset = 1;
 	}
-	return true;
+	return 1;
 }
 
 static gboolean pick_color(GtkWidget  *widget, GdkEvent *event, gpointer data)
@@ -123,21 +121,21 @@ static gboolean pick_color(GtkWidget  *widget, GdkEvent *event, gpointer data)
 	col.blue = c[2];
 	col.alpha = 1;
 	gtk_widget_override_background_color(GTK_WIDGET(gtk.window), GTK_STATE_FLAG_NORMAL, &col);
-	return true;
+	return 1;
 }
 
 static gboolean configure_cb (GtkWidget *widget, GdkEventConfigure *event, gpointer data)
 {
 	GdkPixbuf *buf = (GdkPixbuf*) data;
 	gtk.surface = gdk_cairo_surface_create_from_pixbuf(buf, 1, NULL);
-	return true;
+	return 1;
 }
 
 static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data)
 {
 	cairo_set_source_surface(cr, gtk.surface, 0, 0);
 	cairo_paint(cr);
-	return true;
+	return 1;
 }
 
 static void set_sizes(GdkPixbufLoader *loader, gint width, gint height, gpointer data)
@@ -152,17 +150,17 @@ static void set_sizes(GdkPixbufLoader *loader, gint width, gint height, gpointer
 static void activate(GtkApplication *app, gpointer data)
 {
 	gtk.window = gtk_application_window_new(app);
-	gtk_window_set_resizable(GTK_WINDOW(gtk.window), false);
+	gtk_window_set_resizable(GTK_WINDOW(gtk.window), 0);
 	gtk_container_set_border_width(GTK_CONTAINER(gtk.window), 5);
 
 	GdkPixbufLoader *loader = gdk_pixbuf_loader_new();
 	g_signal_connect(G_OBJECT(loader), "size_prepared", G_CALLBACK(set_sizes), NULL);
 
-	char c;
-	while (std::cin.get(c))
+	unsigned char c;
+	int num;
+	while ((num = read(STDIN_FILENO, &c, 1)) > 0)
 	{
-		unsigned char c2 = static_cast<unsigned char>(c);
-		gdk_pixbuf_loader_write(loader, &c2, 1, NULL);
+		gdk_pixbuf_loader_write(loader, &c, 1, NULL);
 	}
 
 	gdk_pixbuf_loader_close(loader, NULL);
